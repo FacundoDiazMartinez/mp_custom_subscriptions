0b5c5ca6af8a0c338af7dac141a0b6a588c2a6cf
Facundo A. Díaz Martínez <facundo_diaz_martinez@hotmail.com>
2018-07-19 11:08:01 -0300

    fix gemspec errors

--- Rakefile
+++ Rakefile
@@ -4,7 +4,7 @@ require 'echoe'
 
 Echoe.new('mp_custom_subscriptions', '0.1.0') do |p|
   p.description    = "Customized MP subscriptions gem."
-  p.url            = "http://github.com/FacundoDiazMartinez/uniquify"
+  p.url            = "http://github.com/FacundoDiazMartinez/mp_custom_subscriptions"
   p.author         = "Ing. Facundo A. Díaz Martínez"
   p.email          = "facundodiazmartinez@icloud.com"
   p.ignore_pattern = ["tmp/*", "script/*"]
--- suscriptions_generator.rb
+++ suscriptions_generator.rb
@@ -3,12 +3,12 @@ class SuscriptionsGenerator < Rails::Generators::Base
 
   def generate_mailers
   	copy_file "mailers/suscription_mailer.rb", "app/mailers/suscription_mailer.rb"
-  	copy_file "/MercadoPagoLocal.rb", "lib/MercadoPagoLocal.rb"
+  	copy_file "/lib//MercadoPagoLocal.rb", "lib/MercadoPagoLocal.rb"
   	directory "views/suscription_mailers", "app/views/suscription_mailer"
   	copy_file "helpers/mercado_pago_helper.rb", "app/helpers/mercado_pago_helper.rb"
   	copy_file "assets/javascripts/mercado_pago.js", "public/javascripts/mercado_pago.js"
   	copy_file "config/initializers/mercado_pago_local.rb", "config/initializers/mercado_pago_local.rb"
-  	directory "/MercadoPago", "lib/MercadoPago"
+  	directory "/lib/MercadoPago", "lib/MercadoPago"
   	template  "views/layouts/mailer.html.erb", "app/views/layouts/mailer.html.erb"
   end
 
